e3d9167a4e3a9e18d16bc86726943ad8779f78bd
MargotGuo <gyqgdn@126.com>
2020-01-09 22:04:37 +0800

    [Guo Yuanqi]{feat}: delete-action done

--- script/main.js
+++ script/main.js
@@ -1,37 +1,38 @@
 window.onload = () => {
-  axios.get("http://localhost:3000/projects")
-    .then(response => {
-      let project = response.data;
-      rederTask(project);
-      renderStatistic(project);
-    });
+  axios.get("http://localhost:3000/projects").then(response => {
+    renderStatistic(response.data);
+    rederProject(response.data);
+  });
 };
 
-let rederTask = (project) => {
+let rederProject = (project) => {
   project.forEach(item => {
-    document.getElementById("main-table").innerHTML += 
-      `<tr>
+    document.getElementById("tbody").innerHTML += 
+      `<tr id=${item.id}>
         <td class="body-cell">${item.name}</td>
-        <td class="body-cell">
-          <p class="project-detail">${item.description}</p>
-        </td>
+        <td class="body-cell"><p class="project-detail">${item.description}</p></td>
         <td class="body-cell">${item.endTime}</td>
-        <td class="body-cell ${item.status === "ACTIVE" ? "status-active" : (item.status === "PENDING" ? "status-pending" : "status-close")}">${item.status}</td>
-        <td class="body-cell"><button class="delete">删除</button></td>
+        <td class="body-cell status-${item.status.toLowerCase()}">${item.status}</td>
+        <td class="body-cell"><button class="delete" onclick="deleteProject(${item.id})">删除</button></td>
       </tr>`;
   });
 };
 
 let renderStatistic = (project) => {
-  let sum = project.length;
   let countResult = count(project);
+  let sum = project.length;
   document.getElementById("sum").innerHTML = sum;
-  document.getElementById("pending-number").innerHTML = countResult.PENDING;
-  document.getElementById("active-number").innerHTML = countResult.ACTIVE;
-  document.getElementById("close-number").innerHTML = countResult.CLOSED;
-  document.getElementById("pending-percent").innerHTML = `${countResult.PENDING/sum * 100}%`;
-  document.getElementById("active-percent").innerHTML = `${countResult.ACTIVE/sum * 100}%`;
-  document.getElementById("close-percent").innerHTML = `${countResult.CLOSED/sum * 100}%`;
+  ["PENDING", "ACTIVE", "CLOSED"].forEach(status => {
+    let numberInfo = document.getElementById(`${status.toLowerCase()}-number`);
+    let percentInfo = document.getElementById(`${status.toLowerCase()}-percent`);
+    if (countResult[status]) {
+      numberInfo.innerHTML = countResult[status];
+      percentInfo.innerHTML = `${Math.round(countResult[status] / sum * 100)}%`;
+    } else {
+      numberInfo.innerHTML = "0";
+      percentInfo.innerHTML = "0%";
+    }
+  });
 };
 
 let count = (project) => {
@@ -40,4 +41,23 @@ let count = (project) => {
     result[item.status] ? result[item.status]++ : result[item.status] = 1;
   });
   return result;
+};
+
+let deleteProject = (id) => {
+  let alert = document.getElementById("confirm-delete");
+  alert.setAttribute("class", "activate-alert");
+  document.getElementById("confirm").onclick = () => {
+    document.getElementById(id).remove();
+    axios.delete(`http://localhost:3000/projects/${id}`).then(() => {
+      axios.get("http://localhost:3000/projects/").then(response => {
+        renderStatistic(response.data);
+      });
+    });
+    removeFilter();
+  };
+};
+
+let removeFilter = () => {
+  let alert = document.getElementById("confirm-delete");
+  alert.removeAttribute("class");
 };
